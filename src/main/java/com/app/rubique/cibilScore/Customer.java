31ccbb25c66e7a9ecc9bd744f9756c5768631a98
sugyan <sugyansahu84@gmail.com>
2018-09-01 16:18:19 +0530

    rubique-challenge-1

---
+++ src/main/java/com/app/rubique/cibilScore/Customer.java
@@ -0,0 +1,85 @@
+package com.app.rubique.cibilScore;
+
+public class Customer implements Comparable {
+
+    String name;
+    int cibilScore;
+    int riskScore;
+
+    public Customer(String name, int cibilScore, int riskScore) {
+        this.name = name;
+        this.cibilScore = cibilScore;
+        this.riskScore = riskScore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCibilScore() {
+        return cibilScore;
+    }
+
+    public void setCibilScore(int cibilScore) {
+        this.cibilScore = cibilScore;
+    }
+
+    public int getRiskScore() {
+        return riskScore;
+    }
+
+    public void setRiskScore(int riskScore) {
+        this.riskScore = riskScore;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Customer customer = (Customer) o;
+
+        if (cibilScore != customer.cibilScore) return false;
+        if (riskScore != customer.riskScore) return false;
+        return name.equals(customer.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + cibilScore;
+        result = 31 * result + riskScore;
+        return result;
+    }
+
+    @Override
+    public int compareTo(Object other) {
+
+        int retVal = 0;
+
+        Customer otherCustomer = (Customer) other;
+
+        if (this.getCibilScore() > otherCustomer.getCibilScore()) {
+            retVal = -1;
+        } else if (this.getCibilScore() < otherCustomer.getCibilScore()) {
+            retVal = 1;
+        } else {
+            //CIBIL Same.
+            if (this.getRiskScore() > otherCustomer.getRiskScore()) {
+                retVal = 1;
+            } else if (this.getRiskScore() < otherCustomer.getRiskScore()) {
+                retVal = -1;
+            } else {
+                //RISK: Same
+                retVal = (-1) * this.getName().compareTo(otherCustomer.getName());
+            }
+        }
+
+
+        return retVal;
+    }
+}
---
+++ src/main/java/com/app/rubique/cibilScore/TestClass.java
@@ -0,0 +1,43 @@
+package com.app.rubique.cibilScore;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+class TestClass {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        //Scanner s = new Scanner(System.in);
+
+        int T = Integer.parseInt(br.readLine().trim());
+
+        for (int i = 0; i < T; i++) {
+
+            int N = Integer.parseInt(br.readLine().trim());
+
+            List<Customer> list = new ArrayList<Customer>(N);
+
+            for (int j = 0; j < N; j++) {
+                String[] strSplit = br.readLine().trim().split(" ");
+
+                String name = strSplit[0].trim();
+                int cibilScore = Integer.parseInt(strSplit[1].trim());
+                int riskScore = Integer.parseInt(strSplit[2].trim());
+
+                list.add(new Customer(name, cibilScore, riskScore));
+            }
+
+            int k = Integer.parseInt(br.readLine().trim());
+
+            Collections.sort(list);
+
+            if (k-1 >= 0 && k-1 < list.size())
+                System.out.println(list.get(k-1).getName() + " " + list.get(k-1).getCibilScore());
+
+        }
+    }
+}
