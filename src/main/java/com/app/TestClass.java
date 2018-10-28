a2b29a4a8cd965239512c3b14d69211b19544725
sugsahu <sugsahu sugsahu@paypal.com>
2018-10-28 22:05:28 +0530

    Added Sample TestClass to use readily.

---
+++ src/main/java/com/app/TestClass.java
@@ -0,0 +1,54 @@
+package com.app;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.Set;
+
+
+public class TestClass {
+
+    private static BufferedReader br = null;
+    private static Scanner sc = null;
+    private static InputType type;
+    private static PrintStream out;
+
+    private static enum InputType {
+        BUFFERED_READER, SCANNER
+    }
+
+    /**
+     * @param pType:
+     *         sc for Scanner
+     *         br for BufferedReader
+     */
+    public static void initializeReaders(InputType pType) {
+
+        if (pType.equals(InputType.BUFFERED_READER)) {
+            type = pType;
+            br = new BufferedReader(new InputStreamReader(System.in));
+        } else if (pType.equals(InputType.SCANNER)) {
+            type = pType;
+            sc = new Scanner(System.in);
+        }
+        out = System.out;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        initializeReaders(InputType.SCANNER);
+
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+
+        }
+
+    }
+}
+
